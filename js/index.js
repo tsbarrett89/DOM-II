c6c1c5778e8b2dbf9a8a09ac0f91b1d3256ab038
Thomas Barrett <tsbarrett89@gmail.com>
2019-10-01 13:56:53 -0600

    added 4 mouseenter and mouseleave events

--- js/index.js
+++ js/index.js
@@ -1,18 +1,63 @@
 // Your code goes here
-const bottomTitles = document.querySelector('.destination h4');
-const bottomBoxes = document.querySelector('.destination p');
-bottomTitles.addEventListener('click', (event) => {
-    if (bottomBoxes.style.display === 'block'){
-        bottomBoxes.style.display = 'none';
-    } else if (bottomBoxes.style.display === 'none'){
-        bottomBoxes.style.display === 'block';
-    }
+const bottomTitles = document.querySelectorAll('.destination h4');
 
-    console.log('clicked')
-});
+// bottomTitles.forEach((header) => {
+//     const bottomBoxes = this.nextSibling;
+//     header.addEventListener('click', () => {
+//         if (bottomBoxes.style.display === 'block'){
+//         bottomBoxes.style.display = 'none';
+//     } else if (bottomBoxes.style.display === 'none'){
+//         bottomBoxes.style.display === 'block';
+//     }
+
+//     console.log('clicked');
+//     })
+
+// });
+
+
+
+
+// // const bottomBoxes = document.querySelector('.destination p');
+// // bottomTitles.addEventListener('click', (event) => {
+    
+
+// //     console.log('clicked')
+// // });
 
 
 const bottomButtons = document.querySelectorAll('.destination .btn');
 bottomButtons.forEach(button => button.addEventListener('click', () => {
     button.style.backgroundColor = 'red';
 }))
+
+const topImage = document.querySelector('header img');
+topImage.addEventListener('mouseenter', () => {
+    topImage.style.transform = 'scale(0.8)';
+})
+topImage.addEventListener('mouseleave', () => {
+    topImage.style.transform = 'scale(1.0)'
+})
+
+const navBar = document.querySelector('nav');
+navBar.addEventListener('mouseenter', () => {
+    navBar.style.flexDirection = 'row-reverse';
+})
+navBar.addEventListener('mouseleave', () => {
+    navBar.style.flexDirection = 'row'
+})
+
+const changeHeaders = document.querySelectorAll('.text-content h2')
+changeHeaders[0].addEventListener('mouseenter', () => {
+    changeHeaders[0].textContent = `Let's Wait`;
+})
+changeHeaders[0].addEventListener('mouseleave', () => {
+    changeHeaders[0].textContent = `Let's Go!`
+})
+
+changeHeaders[1].addEventListener('mouseenter', () => {
+    changeHeaders[1].textContent = `Boredom Awaits:(`;
+})
+changeHeaders[1].addEventListener('mouseleave', () => {
+    changeHeaders[1].textContent = `Adventure Awaits`
+})
