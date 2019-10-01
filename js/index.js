372b6f6d26bee1e7a0a46cd1794438d79fff82f3
Thomas Barrett <tsbarrett89@gmail.com>
2019-10-01 12:39:57 -0600

    added first event

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
 // Your code goes here
+const bottomTitles = document.querySelector('.destination h4');
+const bottomBoxes = document.querySelector('.destination p');
+bottomTitles.addEventListener('click', (event) => {
+    if (bottomBoxes.style.display === 'block'){
+        bottomBoxes.style.display = 'none';
+    } else if (bottomBoxes.style.display === 'none'){
+        bottomBoxes.style.display === 'block';
+    }
+
+    console.log('clicked')
+});
+
+
+const bottomButtons = document.querySelectorAll('.destination .btn');
+bottomButtons.forEach(button => button.addEventListener('click', () => {
+    button.style.backgroundColor = 'red';
+}))
