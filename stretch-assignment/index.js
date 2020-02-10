aeff9183935d316093a8737f59df8a34c6e98d3a
Thomas Barrett <tsbarrett89@gmail.com>
2020-02-10 16:13:23 -0700

    worked on stretch

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -1,5 +1,45 @@
+const body = document.querySelector('body')
 const order = document.querySelector('.blocks');
 const blocks = document.querySelectorAll('.blocks .block');
+const block = document.getElementsByClassName('block')
+const blockRed = document.getElementsByClassName('block--red');
+const blockBlue = document.getElementsByClassName('block--blue');
+const blockGreen = document.getElementsByClassName('block--green')
+const blockPink = document.getElementsByClassName('block--pink')
+const blockGray = document.getElementsByClassName('block--gray')
+
+
+
+blockRed[0].addEventListener('mousedown', () => {
+    blockRed[0].style.transform = 'translateX(110vw)';
+    blockRed[0].style.transition = 'all 1s'
+});
+
+blockBlue[0].addEventListener('mousedown', () => {
+    blockBlue[0].style.transform = 'translateX(110vw)';
+    blockBlue[0].style.transition = 'all 1s'
+});
+
+blockGreen[0].addEventListener('mousedown', () => {
+    blockGreen[0].style.transform = 'translateX(110vw)';
+    blockGreen[0].style.transition = 'all 1s'
+});
+
+blockPink[0].addEventListener('mousedown', () => {
+    blockPink[0].style.transform = 'translateX(110vw)';
+    blockPink[0].style.transition = 'all 3s'
+});
+
+blockGray[0].addEventListener('mousedown', () => {
+    blockGray[0].style.transform = 'translateX(110vw)';
+    blockGray[0].style.transition = 'all 1s'
+});
+
+body.addEventListener('mouseup', () => {
+    blocks.forEach(block => {
+        block.style.transform = 'translateX(0px)'
+    })
+});
 
 blocks.forEach(block => block.addEventListener('click', () => {
     blocks.forEach(block => block.style.order = (block.style.order + 1));
@@ -7,34 +47,3 @@ blocks.forEach(block => block.addEventListener('click', () => {
 }));
 
 order.style.position = 'relative';
-
-
-// blocks.forEach(block => block.addEventListener('mousedown', () => {
-//     block.style.position = 'relative';
-    
-//     function blastOff (){
-//         var pos = 0;
-//         pos++;
-//         block.style.left = `${pos}px`;
-//     };
-//     setInterval(blastOff, 5);
-    
-// }));
-
-// blocks.forEach(block => block.addEventListener('mouseup', () => {
-//     block.style.position = 'static';
-//     block.style.left = '0px';
-// }))
-
-
-// blocks.forEach(block => block.addEventListener('mousedown', () => {
-//     TweenMax.to(".block", 10, {x:800})
-// }));
-
-// blocks.forEach(block => block.addEventListener('mouseup', () => {
-//     TweenMax.from(".block", 10, {x:800})
-// }));
-
-blocks[0].addEventListener('mousedown', () => {
-    TweenMax.to("blocks", 10, {x:800})
-});
